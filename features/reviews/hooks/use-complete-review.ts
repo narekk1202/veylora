2b486419bffa970d3deb726309a106b16c55859e
narekk1202 <narekdunamaliiian@gmail.com>
2026-08-27 11:25:16 +0400

    Refactor `useCompleteReview` hook by removing unused `useRouter` import and updating `EmptyReviews` component to change heading style from medium to semibold for improved UI consistency.

--- features/reviews/hooks/use-complete-review.ts
+++ features/reviews/hooks/use-complete-review.ts
@@ -1,6 +1,5 @@
 import { toast } from "@/shared/components/ui/toast";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { completeReview } from "../actions";
@@ -8,7 +7,6 @@ import { completeReviewSchema, CompleteReviewSchema } from "../schemas";
 
 export const useCompleteReview = (reviewId: string) => {
   const [isPending, startTansition] = useTransition();
-  const router = useRouter();
 
   const form = useForm<CompleteReviewSchema>({
     resolver: zodResolver(completeReviewSchema),
--- features/reviews/ui/empty-reviews.tsx
+++ features/reviews/ui/empty-reviews.tsx
@@ -1,7 +1,7 @@
 const EmptyReviews = ({ message }: { message: string }) => {
   return (
     <div className="mt-6 flex flex-col items-center justify-center py-12">
-      <h2 className="text-foreground text-center font-serif text-2xl font-medium">
+      <h2 className="text-foreground text-center font-serif text-2xl font-semibold">
         Nothing here
       </h2>
       <p className="text-muted-foreground mt-1 text-center text-sm">{message}</p>
